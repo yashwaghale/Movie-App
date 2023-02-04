f8c76958df66318a9d15c7344ebce2746237d61d
yash waghale <waghaleyash44@gmail.com>
2023-02-04 12:24:35 +0530

    Updates

---
+++ precache-manifest.8998249b7c69395c43fb6d0673f5a0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4012abda106013bbe08f",
+    "url": "/movie_App/static/css/main.5cf75280.chunk.css"
+  },
+  {
+    "revision": "4012abda106013bbe08f",
+    "url": "/movie_App/static/js/main.4012abda.chunk.js"
+  },
+  {
+    "revision": "1508509f100203b6bf04",
+    "url": "/movie_App/static/css/1.6f8aebae.chunk.css"
+  },
+  {
+    "revision": "1508509f100203b6bf04",
+    "url": "/movie_App/static/js/1.1508509f.chunk.js"
+  },
+  {
+    "revision": "196a8e50fe635d0f73af",
+    "url": "/movie_App/static/js/runtime~main.196a8e50.js"
+  },
+  {
+    "revision": "a36f8daec5bf3275c2183f1204a8f6db",
+    "url": "/movie_App/index.html"
+  }
+];
